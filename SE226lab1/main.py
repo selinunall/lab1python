c253b59ebfea81e189a6176c902ed79cba557e99
Şenay Selin Ünal <Seln01unnal@gmail.com>
2023-03-07 15:04:02 +0300

    lab1python
    lab1python

---
+++ SE226lab1/main.py
@@ -0,0 +1,40 @@
+#Question1
+name=input('What is your name?')
+print(name)
+
+print('Hello Ege')
+ID=input('What is your student ID?')
+print(ID)
+print('Your ID is ')
+print(ID)
+
+#Question2
+var1= int(input('Enter a value for var1:'))
+var2=int(input('Enter a value for var2:'))
+
+sum=var1+var2
+diff=var1-var2
+prod=var1*var2
+
+print(var1)
+print(var2)
+print(sum)
+print(diff)
+print(prod)
+
+#Question3
+name=input('Enter your name:')
+labGrade=float(input('Enter your lab grade:'))
+midtermGrade=float(input('Enter your midterm grade:'))
+finalGrade=float(input('Enter your final grade:'))
+
+lastScore=float((labGrade*0.25)+(midtermGrade*0.35)+(finalGrade*0.4))
+
+print('Name: '+name)
+print('Lab: '+str(labGrade))
+print('Midterm: '+str(midtermGrade))
+print('Final: '+str(finalGrade))
+print('Last Score :'+str(lastScore))
+
+#Question4
+print('*\n**\n***\n**\n*')
